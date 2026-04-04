8d5ef6bd58bffbce8766e4707ff6c0281896ffa0
Adilov Timur <adilovtimur14@gmail.com>
2026-04-04 11:55:43 +0500

    Вся логика аутентификации

---
+++ server/config.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const JWT_SECRET = process.env.JWT_SECRET;
+module.exports = {
+  secret: JWT_SECRET,
+};
--- server/index.js
+++ server/index.js
@@ -1,3 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./authRouter');
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    app.listen(PORT, () => console.log(`Cервер работает на порту ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
